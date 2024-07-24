7f3abe94aa563383bf65de342610239320290c66
anubhav0107 <124803383+anubhav0107@users.noreply.github.com>
2024-07-23 21:55:36 -0700

    pass 2nd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,56 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-    //  Uncomment this block to pass the first stage
+        //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
         try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-          clientSocket.getOutputStream().write("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
+            serverSocket = new ServerSocket(port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+
+            clientSocket = serverSocket.accept();
+
+            respondToPing(clientSocket);
+
         } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-          }
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
-  }
+    }
+
+    private static void respondToPing(Socket clientSocket){
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try{
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            String nextLine = "";
+            while((nextLine = in.readLine()) != null){
+                if(nextLine.equalsIgnoreCase("PING")){
+                    out.print("+PONG\r\n");
+                    out.flush();
+                }
+            }
+        }catch(IOException e){
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
