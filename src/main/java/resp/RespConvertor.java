8b1cf09dcaab051f8c7037150addbc4566e63f8c
anubhav0107 <124803383+anubhav0107@users.noreply.github.com>
2024-07-30 11:54:36 -0700

    CLI tests (cli-test-1722365676816)

--- src/main/java/resp/RespConvertor.java
+++ src/main/java/resp/RespConvertor.java
@@ -1,8 +1,12 @@
 package resp;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RespConvertor {
+
     public static String toBulkString(String input){
         if (input == null) {
             return "$-1\r\n";  // Null bulk string
@@ -10,11 +14,14 @@ public class RespConvertor {
         return "$" + input.length() + "\r\n" + input + "\r\n";
     }
 
-    public static String toRESPArray(List<String> input){
+    public static String toRESPArray(List<String> input, boolean bulkConversionRequired){
         int len = input.size();
         StringBuilder output = new StringBuilder(String.format("*%d\r\n", len));
         for(String in : input){
-            output.append(toBulkString(in));
+            if(bulkConversionRequired){
+                in = toBulkString(in);
+            }
+            output.append(in);
         }
         return output.toString();
     }
