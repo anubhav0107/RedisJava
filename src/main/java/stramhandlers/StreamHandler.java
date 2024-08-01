fede43fdcdf813bf963914b717b7de3239bec6be
anubhav0107 <124803383+anubhav0107@users.noreply.github.com>
2024-08-01 16:18:46 -0700

    pass stage

--- src/main/java/stramhandlers/StreamHandler.java
+++ src/main/java/stramhandlers/StreamHandler.java
@@ -6,6 +6,8 @@ import redisdatastructures.stream.RedisStream;
 import redisdatastructures.stream.Stream;
 import resp.RespConvertor;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,7 +19,8 @@ public class StreamHandler {
 
             String streamKey = (String) list.get(1);
             String entryKey = (String) list.get(2);
-            if(entryKey == "*"){
+
+            if (entryKey.equals("*")) {
                 entryKey = "*-*";
             }
             String entryId = entryKey.split("-")[0];
@@ -30,24 +33,27 @@ public class StreamHandler {
             if (RedisStream.containsKey(streamKey) && !validateStream(RedisStream.getStream(streamKey), entryId, entrySequence)) {
                 return RespConvertor.toErrorString("The ID specified in XADD is equal or smaller than the target stream top item");
             }
-
             Stream stream = RedisStream.getStream(streamKey);
             Entries entries;
-            if(entryKey.equals("*")){
-                entries = stream.addEntries();
-            }else{
-                entries = stream.getEntries(Long.parseLong(entryId));
+            long entryIdLong;
+            if (entryId.equals("*")) {
+                entryIdLong = Timestamp.valueOf(LocalDateTime.now()).getTime();
+            } else {
+                entryIdLong = Long.parseLong(entryId);
             }
+
+            entries = stream.getEntries(entryIdLong);
+
             Map<String, String> entry = prepareMap(list.subList(3, list.size()));
 
             long newSequence = 0;
-            if(entrySequence.equals("*")){
-                newSequence = entries.addEntry(entry, Long.parseLong(entryId));
-            }else{
+            if (entrySequence.equals("*")) {
+                newSequence = entries.addEntry(entry, entryIdLong);
+            } else {
                 newSequence = entries.addEntry(Long.parseLong(entrySequence), entry);
             }
             RedisKeys.addKey(streamKey, "stream");
-            return  RespConvertor.toBulkString(entryId + "-" + newSequence);
+            return RespConvertor.toBulkString(entryIdLong + "-" + newSequence);
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -56,20 +62,20 @@ public class StreamHandler {
     }
 
     private static boolean validateStream(Stream stream, String entryId, String entrySequence) {
-        if(entryId.equals("*") || entrySequence.equals("*")){
+        if (entryId.equals("*") || entrySequence.equals("*")) {
             return true;
         }
         Long lastEntries = stream.getLastId();
-        if(lastEntries > Long.parseLong(entryId)){
+        if (lastEntries > Long.parseLong(entryId)) {
             return false;
-        }else if(lastEntries == Long.parseLong(entryId) && stream.getEntries(lastEntries).getLastSequence() >=  Long.parseLong(entrySequence)){
+        } else if (lastEntries == Long.parseLong(entryId) && stream.getEntries(lastEntries).getLastSequence() >= Long.parseLong(entrySequence)) {
             return false;
         }
         return true;
     }
 
     private static boolean validateEntryKey(String entryId, String entrySequence) {
-        if(entryId.equals("0") && entrySequence.equals("0")){
+        if (entryId.equals("0") && entrySequence.equals("0")) {
             return false;
         }
         return true;
@@ -77,8 +83,8 @@ public class StreamHandler {
 
     private static Map<String, String> prepareMap(List<Object> list) {
         Map<String, String> pairMap = new HashMap<>();
-        for(int i = 0, j = i + 1; i < list.size() - 1; i = i + 2){
-            pairMap.put((String)list.get(i), (String)list.get(j));
+        for (int i = 0, j = i + 1; i < list.size() - 1; i = i + 2) {
+            pairMap.put((String) list.get(i), (String) list.get(j));
         }
         return pairMap;
     }
