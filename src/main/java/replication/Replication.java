180dc888b3556c85884f7ec0532e9c43c34dd843
anubhav0107 <124803383+anubhav0107@users.noreply.github.com>
2024-08-19 10:36:32 -0700

    pass stage

--- src/main/java/replication/Replication.java
+++ src/main/java/replication/Replication.java
@@ -17,50 +17,50 @@ public class Replication {
         try {
             Socket socket = new Socket(ReplicationConfig.getMasterIP(), Integer.parseInt(ReplicationConfig.getMasterPort()));
             StringBuilder response = new StringBuilder();
-            try (PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-                String ping = RespConvertor.toRESPArray(List.of("PING"), true);
-                out.print(ping);
-                out.flush();
-                // Read the server's response
-                String line = in.readLine();
+            String ping = RespConvertor.toRESPArray(List.of("PING"), true);
+            out.print(ping);
+            out.flush();
+            // Read the server's response
+            String line = in.readLine();
 
-                String replConf1 = RespConvertor.toRESPArray(List.of("REPLCONF", "listening-port", String.valueOf(port)), true);
-                out.print(replConf1);
-                out.flush();
-                line = in.readLine();
+            String replConf1 = RespConvertor.toRESPArray(List.of("REPLCONF", "listening-port", String.valueOf(port)), true);
+            out.print(replConf1);
+            out.flush();
+            line = in.readLine();
 
-                String replConf2 = RespConvertor.toRESPArray(List.of("REPLCONF", "capa", "psync2"), true);
-                out.print(replConf2);
-                out.flush();
-                line = in.readLine();
+            String replConf2 = RespConvertor.toRESPArray(List.of("REPLCONF", "capa", "psync2"), true);
+            out.print(replConf2);
+            out.flush();
+            line = in.readLine();
 
-                String pSync = RespConvertor.toRESPArray(List.of("PSYNC", "?", "-1"), true);
-                out.print(pSync);
-                out.flush();
-                line = in.readLine();
+            String pSync = RespConvertor.toRESPArray(List.of("PSYNC", "?", "-1"), true);
+            out.print(pSync);
+            out.flush();
+            line = in.readLine();
 
-                new Thread(() -> listenToSocket(socket, in)).start();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
+            BufferedReader in2 = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            new Thread(() -> listenToSocket(socket, in2)).start();
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    private static void listenToSocket(Socket socket, BufferedReader in){
-        try{
+    private static void listenToSocket(Socket socket, BufferedReader in) {
+        try {
             RespParser rp = new RespParser(in);
             while (!socket.isClosed()) {
                 Object object = rp.parse();
                 if (object == null) {
                     continue;
                 }
+                System.out.println("Object Not Null: ");
                 handleParsedRESPObject(object);
             }
-        } catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
@@ -69,7 +69,7 @@ public class Replication {
         if (object instanceof List) {
             List<Object> list = (List<Object>) object;
             String command = (String) list.get(0);
-
+            System.out.println("Command: " + command);
             switch (command.toUpperCase()) {
                 case "SET":
                     ClientHandler.handleSet(list);
